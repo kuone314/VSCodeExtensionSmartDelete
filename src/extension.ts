e1f520aed1ca4f1925837162df26f27f3ffb86a9
kuone <kuone@example.com>
2022-08-18 11:54:06 +0900

    [env] 不要な一時変数の削除

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,10 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('smart-delete.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from smart delete!');
-	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(
+		vscode.commands.registerCommand('smart-delete.helloWorld', () => {
+			vscode.window.showInformationMessage('Hello World from smart delete!');
+		})
+	);
 }
